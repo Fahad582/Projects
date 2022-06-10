25a852dc4bf9b8dd26eabc852d5566444142cbbb
FahadTariq <Saadtariq05@gmail.com>
2022-06-10 19:27:42 +0500

    update

---
+++ Assignmentproject/Assignmentproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignmentproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
